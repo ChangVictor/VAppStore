bfb7b70b27dbeb23dd8c58ff82b3aef337146bc3
Victor Chang <v.chang.yu@gmail.com>
2019-03-11 17:48:41 -0300

    nested horizontal collectionView

--- VAppStore/Controllers/Apps/AppsController.swift
+++ VAppStore/Controllers/Apps/AppsController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class AppsController: BaseListController {
+class AppsController: BaseListController, UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "id"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.backgroundColor = .yellow
+        collectionView.register(AppsGroupCell.self, forCellWithReuseIdentifier: "id")
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 250)
+    }
+    
 
 }
---
+++ VAppStore/Views/Apps/AppsGroupCell.swift
@@ -0,0 +1,43 @@
+//
+//  AppsGroupCell.swift
+//  VAppStore
+//
+//  Created by Victor Chang on 11/03/2019.
+//  Copyright © 2019 Victor Chang. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    convenience init(text: String, font: UIFont) {
+        self.init(frame: .zero)
+        self.text = text
+        self.font = font
+    }
+}
+
+class AppsGroupCell: UICollectionViewCell {
+    
+    let titleLabel = UILabel(text: "Apps Section", font: .boldSystemFont(ofSize: 30))
+    
+    let horizontalController = AppsHorizontalController()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .lightGray
+        
+        addSubview(titleLabel)
+        titleLabel.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor)
+        
+        addSubview(horizontalController.view)
+        horizontalController.view.backgroundColor = .blue
+        horizontalController.view.anchor(top: titleLabel.bottomAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
